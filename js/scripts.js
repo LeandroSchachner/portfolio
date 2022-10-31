16c8129837cc88964a9b1143ec23a132b01e2496
Leandro Schachner <87399673+LeandroSchachner@users.noreply.github.com>
2022-10-31 18:48:07 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
-/*!
-* Start Bootstrap - Stylish Portfolio v6.0.5 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
-*/
-window.onload = function(){
-    var contenedor = document.getElementById('carga-container');
-    
-    contenedor.style.visibility = "hidden";
-    contenedor.style.opacity = '0';
-}
+
+
 window.addEventListener('DOMContentLoaded', event => {
 
     const sidebarWrapper = document.getElementById('sidebar-wrapper');
